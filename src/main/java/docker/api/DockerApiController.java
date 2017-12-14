1466a2b2e87d38a3c8eb4b36c80f3d48c0ddda23
Travis Kosarek <travis.kosarek@gmail.com>
2017-12-14 16:28:18 -0600

    hacky fix for windows, does it work for linux?

--- src/main/java/docker/api/DockerApiController.java
+++ src/main/java/docker/api/DockerApiController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.qos.logback.core.joran.conditional.ElseAction;
 import docker.api.container.Container;
 import docker.api.node.Node;
 import docker.api.service.Service;
@@ -58,8 +59,17 @@ public class DockerApiController {
         ArrayList<String> output = new ArrayList<String>();
         ArrayList<String> errorOutput = new ArrayList<String>();
 
-        final Process p = Runtime.getRuntime().exec(new String[]{"bash", "-c", command});
-        
+        final Process p;
+
+        if(OperatingSystem.isUnix() || OperatingSystem.isMac() || OperatingSystem.isSolaris())
+        {
+            p = Runtime.getRuntime().exec(new String[] { "bash", "-c", command });
+        }
+        else
+        {
+            p = Runtime.getRuntime().exec(command);
+        }
+
         new Thread(new Runnable() {
             public void run() {
                 BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
@@ -95,7 +105,7 @@ public class DockerApiController {
     protected Swarm getSwarm() throws Exception {
         String name = "";
 
-        String command = "docker stack ls --format '{{.Name}}'";
+        String command = "docker stack ls --format \"{{.Name}}\"";
         ArrayList<String> output = this.getTerminalOutput(command);
 
         if (!output.isEmpty()) {
@@ -107,8 +117,8 @@ public class DockerApiController {
 
     protected ArrayList<Node> getNodes() throws Exception {
 
-        String command = "docker node ls --format '{{.ID}} {{.Hostname}} {{.Status}} {{.Availability}} {{.ManagerStatus}}'" ;
-        
+        String command = "docker node ls --format \"{{.ID}} {{.Hostname}} {{.Status}} {{.Availability}} {{.ManagerStatus}}\"";
+
         ArrayList<String> output = this.getTerminalOutput(command);
 
         ArrayList<Node> nodes = new ArrayList<Node>();
@@ -127,8 +137,9 @@ public class DockerApiController {
         ArrayList<Service> services = new ArrayList<Service>();
 
         if (!swarmName.equals("")) {
-            String command = String.format("%s %s %s","docker stack services", swarmName, "--format '{{.ID}} {{.Mode}} {{.Replicas}} {{.Image}}'"); 
-            
+            String command = String.format("%s %s %s", "docker stack services", swarmName,
+                    "--format \"{{.ID}} {{.Mode}} {{.Replicas}} {{.Image}}\"");
+
             ArrayList<String> output = this.getTerminalOutput(command);
 
             if (!output.isEmpty()) {
@@ -147,8 +158,9 @@ public class DockerApiController {
         ArrayList<Container> containers = new ArrayList<Container>();
 
         if (!serviceID.equals("")) {
-            String command = String.format("%s %s %s","docker service ps", serviceID, "--format '{{.Name}} {{.Image}} {{.Node}} {{.CurrentState}}'");
-            
+            String command = String.format("%s %s %s", "docker service ps", serviceID,
+                    "--format \"{{.Name}} {{.Image}} {{.Node}} {{.CurrentState}}\"");
+
             ArrayList<String> output = this.getTerminalOutput(command);
 
             ArrayList<String> alreadyFoundContainers = new ArrayList<String>();
@@ -172,6 +184,29 @@ public class DockerApiController {
 
         public CommandOutputException(String message) {
             super(message);
+        }
+    }
+
+    protected static class OperatingSystem {
+
+        private static String OS = System.getProperty("os.name").toLowerCase();
+
+        public OperatingSystem() {}
+
+        public static boolean isWindows() {
+            return (OS.indexOf("win") >= 0);
+        }
+
+        public static boolean isMac() {
+            return (OS.indexOf("mac") >= 0);
+        }
+
+        public static boolean isUnix() {
+            return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0);
+        }
+
+        public static boolean isSolaris() {
+            return (OS.indexOf("sunos") >= 0);
         }
     }
 }
